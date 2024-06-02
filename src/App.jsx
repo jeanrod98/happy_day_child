9226400d1cc055ef7efc7da68222be266852bfec
jeanrod28 <jcrod.jc@gmail.com>
2024-06-02 18:36:54 -0500

    se agrego una carta

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Desafios from './Components/Desafios'
 import styled from 'styled-components'
+import HappyChild from './screens/HappyChild'
+import Leters from './screens/Leters'
 
 function App() {
-  const [mostrarDesafios, setMostrarDesafios] = useState(false)
 
   return (
+    
     <>
-      <Contenedor>
-        <h1>Hoy se celebra el Día del Niño</h1>
-        <p>Vamos a comprobar si te mereces la sorpresa &#128522; </p>
-        <button onClick={(e) => setMostrarDesafios(true)}>Acepto el desafío!</button>
-       
-      </Contenedor>
-     
-     {
-      mostrarDesafios && <Desafios setMostrarDesafios={setMostrarDesafios}/>
-     }
+    {/* <HappyChild/> */}
+    <Leters/>
+
+
     </>
   )
 }
 
 export default App
 
-const Contenedor = styled.div`
-
-  h1{
-    color: #ac35ff;
-    text-align: center;
-
-  }
-
-  p{
-    color: #6A8BFF;
-  }
-  
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  
-  p{
-    font-size: 24px;
-    text-align: center;
-  }
-`;
